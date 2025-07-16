6ca26eeb5785dca5e464eda719b61f71766f8f06
VarshuVarshney <160710211+VarshuVarshney@users.noreply.github.com>
2025-07-16 16:58:18 +0530

    Add .next/static/chunks/src_app_page_tsx_6070293a._.js 1752665300363

---
+++ .next/static/chunks/src_app_page_tsx_6070293a._.js
@@ -0,0 +1,8 @@
+(globalThis.TURBOPACK_CHUNK_LISTS = globalThis.TURBOPACK_CHUNK_LISTS || []).push({
+    script: typeof document === "object" ? document.currentScript : undefined,
+    chunks: [
+  "static/chunks/src_d74d7c2e._.js",
+  "static/chunks/node_modules_9ace8bbf._.js"
+],
+    source: "dynamic"
+});
